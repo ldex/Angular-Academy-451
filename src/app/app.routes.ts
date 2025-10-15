025441648e0fabc00e0629633a8430ff494ee4aa
ldex <laurent.duveau+git@gmail.com>
2025-10-15 12:04:57 -0400

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/home.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ContactComponent } from './shared/contact.component';
+import { AdminComponent } from './shared/admin.component';
+import { ErrorComponent } from './shared/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'products', component: ProductListComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: '**', component: ErrorComponent }
+];
